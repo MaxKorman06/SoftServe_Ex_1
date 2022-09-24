a608fa25631ee4e29204724dca11cbd2bc7b310c
Максим Корман <mkorman359@gmail.com>
2022-09-24 21:14:35 +0300

    V1.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int century(int year)
+    {
+        int century  = (year / 100) + 1;
+        if (year % 100 == 0)
+        {
+            century -= 1;
+        }
+        if (year == 0)
+        {
+            century = 0;
+        }
+        return century;
     }
-    // 2eqweqwfqvasaesc123124124124
+    public static void main(String[] args) throws IOException
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(century(scanner.nextInt()));
+    }
 }
